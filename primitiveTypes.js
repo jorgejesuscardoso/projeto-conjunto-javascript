212e8d05dab9ba993c65b242ad0cb80fb3dd1219
unknown <tst.jorgecardoso@gmail.com>
2025-03-25 16:34:38 -0300

    obj

--- primitiveTypes.js
+++ primitiveTypes.js
@@ -1,13 +1,29 @@
-const number = 10;
+const number = '10';
 const string = 'Hello World';
 const boolean = true;
 const nullValue = null;
 const undefinedValue = undefined;
 const object = { key: 'value' };
+const array = [1, 2, 3];
+
+const arrayObject = [
+    { key: 'value' },
+    { key: 'value' }
+];
+
+const object2 = { 
+    keyNumber: 10,
+    keyString: 'Hello World',
+    keyBoolean: true,
+    keyNull: null,
+    keyUndefined: undefined,
+    keyArray: [1, 2, 3],
+    keyObject: { key: 'value' }
+ };
 
 console.log(typeof number); // number
-console.log(typeof string); // string
-console.log(typeof boolean); // boolean
-console.log(typeof nullValue); // object
-console.log(typeof undefinedValue); // undefined
-console.log(typeof object); // object
+// console.log(typeof string); // string
+// console.log(typeof boolean); // boolean
+// console.log(typeof nullValue); // object
+// console.log(typeof undefinedValue); // undefined
+// console.log(typeof object); // object
